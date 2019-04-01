e6230e71469f5258c179a84a27aa0b85219ea616
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 05:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (shout(string) === string) {
-    return `YES INDEED!`
+    return "YES INDEED!"
   } else if (whisper(string) === string) {
-  return `I can\`t hear you!`
+  return "I can't hear you!"
   } else if (string === `I love you, Grandma.`) {
-  return `I love you, too.`
+  return "I love you, too."
 }
 }
 
