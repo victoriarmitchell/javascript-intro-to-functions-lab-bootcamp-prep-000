9e7eb6f9b9e18a39942443334ca434efe5d00583
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 05:00:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === lowercase) {
-    return `I can't hear you
+    return `I can't hear you!`
   }
-  else if string === 'I love you, Grandma.'
+  else if (string === `I love you, Grandma.`) {
+    return `I love you too
+  }
   else string === uppercase
   return `I can't hear you`;
 }
