31dcb146c79e36dc11a4330c5075bc379c8606d6
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 05:06:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-var string = `HELLO`;
+var uppercase = "HELLO!"
+ 
+uppercase.toUpperCase() === uppercase // true
+ 
+var lowercase = 'hello!'
+ 
+lowercase.toLowerCase() === lowercase // true
+ 
+var mixedCase = 'Hi there!'
+ 
+mixedCase.toLowerCase() === mixedCase // false
+ 
+mixedCase.toUpperCase() === mixedCase // false
 function shout(string) {
   return string.toUpperCase();
 }
