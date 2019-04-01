aaa7b1956eab3ae9da33b1f2171904da249f9fd4
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 05:22:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var string = 'HELLO'
 var uppercase = "HELLO!"
  
 uppercase.toUpperCase() === uppercase // true
@@ -29,5 +28,23 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+  if (string.toUpperCase === string) {
+  return `YES INDEED!`
+  else if (string.toLowerCase === string)
+  return `I can\`t hear you!`
+  else
 }
+
+describe('sayHiToGrandma(string)', function() {
+  it('returns "I can\'t hear you!" if `string` is lowercase', function() {
+    expect(sayHiToGrandma('hello')).toEqual("I can't hear you!")
+  })
+
+  it('returns "YES INDEED!" if `string` is uppercase', function() {
+    expect(sayHiToGrandma('HELLO')).toEqual("YES INDEED!")
+  })
+
+  it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
+    expect(sayHiToGrandma("I love you, Grandma.")).toEqual("I love you, too.")
+  })
+})
