762fd9c542df93a3f9f896a755317672d2a38554
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 04:57:44 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+var string = `HELLO`;
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(shout(string));
+}
+
+function logWhisper(string) {
+  console.log(whisper(string));
+}
+
+function sayHiToGrandma(string) {
+  if string === lowercase;
+  else if string === 'I love you, Grandma.'
+  else string === uppercase
+  return `I can't hear you`;
+}
