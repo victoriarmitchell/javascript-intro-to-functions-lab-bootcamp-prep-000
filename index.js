e60adbc8e7a509dccc6eb81b1fd25b13c4e9ae71
Victoria M <victoria.raq.mitchell@gmail.com>
2019-04-01 05:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,6 +32,8 @@ function sayHiToGrandma(string) {
     return `YES INDEED!`
   } else if (string.toLowerCase === string) {
   return `I can\`t hear you!`
-  } else if (string === )
+  } else if (string === `I love you, Grandma.`) {
+  return `I love you, too.`
+}
 }
 
